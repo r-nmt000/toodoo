64e992bb402873058ada3f4097c714a05c0eaabb
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-08-04 23:04:24 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import aws_export from './src/aws-exports';
+
+Amplify.configure(aws_export);
 
 export default function App() {
   return (
